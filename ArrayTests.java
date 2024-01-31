68aa0c057ab873f5033c16cd84a0f2dd8a7fe520
bryab-edu <bcanonizado@ucsd.edu>
2024-01-31 15:20:31 -0800

    Fixed methods and added testcases in ArrayTests and ArrayExamples
    Fixed methods reversed() and reversedInPlace().

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -14,5 +15,20 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedFilled(){
+    int[] input2 = {1, 2, 3};
+    int[] input2_reversed = {3, 2, 1};
+    assertArrayEquals(input2_reversed, ArrayExamples.reversed(input2));
+  }
+
+  @Test
+  public void testReversedInPlaceFilled(){
+    int[] input2 = {1, 2, 3};
+    int[] input2_reversed = {3, 2, 1};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(input2_reversed, input2);
   }
 }
